cfc41f5b23737a0fc911bdca6d84fa07d9901686
hidoos <sk8huaban@gmail.com>
2024-01-13 21:51:55 +0800

    使用js构建vdom的对象，使用dom来渲染vdom

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
-// v0 - vdom - 渲染 vdom
-// 描述vdom 的数据结构
-// 使用数据结构的函数
+// 使用 type 类型的不同去拆分
 
+const createTextNode = (text) => {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
 
-// 构建 vdom 的数据结构
-// 产生了两个问题
-// 对于 children 的，是创建子元素，还是创建文本节点，应该使用什么来判断
-const createElement = (type, props, ...children) => {
+const element = {
+}
+
+const createElement = (type, props,...children) => {
   return {
     type,
     props: {
       ...props,
-      children 
-    },
-  }  
+      children
+    }
+  }
 }
+
 
 
 // 使用 native dom 来渲染创建好的数据结构
 
-const render = () => {
-  const el = document.createElement(type)
-  el.id = props.id
-  el.textContent = text
-  return el
+
+const render = (el, container) => {
+  // 创建元素的type
+  const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode(''): document.createElement(el.type)
+  Object.keys(el.props).forEach(key => {
+    if(key !== 'children') {
+      dom[key] = el.props[key]
+    }
+  })
+  const children = el.props.children
+  children.forEach((child) => {
+    render(child, dom)
+  })
+  container.append(dom)
 }
 
-const entryEl = document.getElementById("root")
-entryEl.append(createElement('div', {id: 'app'}, 'App'))
+const container = document.getElementById("root")
+const App = createElement('div', {id: 'app'}, createTextNode('hello App'))
+console.log("App", App)
+render(App, container)
