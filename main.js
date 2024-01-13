f435e55bf22b65b142413007454bf92986d4eca5
hidoos <sk8huaban@gmail.com>
2024-01-13 21:01:58 +0800

    卡住了，想要先描述 vdom 的数据结构，但是数据结构应该怎么构造这个部分卡住了

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-const entryEl = document.getElementById("root")
-
-// v0 - vdom -
+// v0 - vdom - 渲染 vdom
 // 描述vdom 的数据结构
 // 使用数据结构的函数
 
-createElement = () => {
-  const el = document.createElement('div')
-  el.id = "app"
-  el.textContent = "App"
+
+// 构建 vdom 的数据结构
+// 产生了两个问题
+// 对于 children 的，是创建子元素，还是创建文本节点，应该使用什么来判断
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children 
+    },
+  }  
+}
+
+
+// 使用 native dom 来渲染创建好的数据结构
+
+const render = () => {
+  const el = document.createElement(type)
+  el.id = props.id
+  el.textContent = text
   return el
 }
 
-entryEl.append(createElement())
+const entryEl = document.getElementById("root")
+entryEl.append(createElement('div', {id: 'app'}, 'App'))
