ba4f2ba1703e9480af1314796ddae8cfc1409d1d
hidoos <sk8huaban@gmail.com>
2024-01-15 23:45:58 +0800

    refactor: 消除多余的分支

--- v4-functional-component/core/React.js
+++ v4-functional-component/core/React.js
@@ -16,7 +16,7 @@ const createElement = (type, props, ...children) => {
       children: children.map(child => {
         console.log(child) // 会发现这里 child 直接输入 10 了, 应该想办法把这里当作一个文本节点来处理
         const isTextNode = typeof child === 'string' || typeof child === 'number'
-        return isTextNode ? createTextNode(child): child
+        return isTextNode ? createTextNode(child) : child
       })
     }
   }
@@ -26,13 +26,13 @@ let root = null
 let nextUnitOfWork = null
 function workLoop(deadline) {
   let shouldYield = false
-  while(!shouldYield && nextUnitOfWork) {
+  while (!shouldYield && nextUnitOfWork) {
     // 执行完当前任务，需要返回下一个任务
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
     shouldYield = deadline.timeRemaining() < 1
   }
 
-  if(!nextUnitOfWork && root) {
+  if (!nextUnitOfWork && root) {
     // 1. 当前链表已经处理完
     commitRoot()
   }
@@ -46,7 +46,7 @@ function commitRoot() {
 }
 
 function commitWork(fiber) {
-  if(!fiber) return
+  if (!fiber) return
 
   // 这里因为 counter 是一个函数组件，本身并不是work单元，没有fiber相关的dom，他直接返回的就是一个 vdom
   // 所以在这里通过向上查找的方式，通过parent并且有dom的情况来 append
@@ -58,8 +58,8 @@ function commitWork(fiber) {
   }
 
   // counter 函数组件 fiber，没有 dom，需要检测一下
-  if(fiber.dom) {
-    fiberParent.dom.append(fiber.dom) 
+  if (fiber.dom) {
+    fiberParent.dom.append(fiber.dom)
   }
 
   commitWork(fiber.child)
@@ -102,31 +102,47 @@ function updateProps(props, dom) {
   })
 }
 
+function updateFunctionComponent(fiber) {
+  const children = [fiber.type(fiber.props)]
+  // 3 转换tree为链表，设置好指针
+  initChildren(fiber, children)
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type))
+    updateProps(fiber.props, dom)
+  }
+  const children = fiber.props.children
+  // 3 转换tree为链表，设置好指针
+  initChildren(fiber, children)
+}
+
 function performUnitOfWork(fiber) {
   const isFunctionComponent = typeof fiber.type === 'function'
 
-  if(!isFunctionComponent) {
-    if(!fiber.dom) {
-      const dom = (fiber.dom = createDom(fiber.type))
-      updateProps(fiber.props, dom)
-    }
+  if (!isFunctionComponent) {
+    updateHostComponent(fiber)
+  } else {
+    updateFunctionComponent(fiber)
   }
-  const children = isFunctionComponent ? [fiber.type(fiber.props)]:  fiber.props.children
+
+  const children = isFunctionComponent ? [fiber.type(fiber.props)] : fiber.props.children
   // 3 转换tree为链表，设置好指针
   initChildren(fiber, children)
 
   // 4 返回下一个要执行的任务
-  if(fiber.child) {
+  if (fiber.child) {
     return fiber.child
   }
-  if(fiber.sibling ) {
+  if (fiber.sibling) {
     return fiber.sibling
   }
 
   let nextFiber = fiber
-  while(nextFiber) {
-   if(nextFiber.sibling) return nextFiber.sibling
-   nextFiber = nextFiber.parent
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling
+    nextFiber = nextFiber.parent
   }
 }
 
